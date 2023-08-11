4f9ca3d030e6252ae0e9949dfd3ce5441bbf394d
Your Name <you@example.com>
2023-08-11 15:41:37 +0300

    created redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import FavoritesPage from "./pages/FavoritesPage";
+import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
-    <div className="bg-slate-500 min-h-screen text-center">
-      <p className="text-white p-20">Hello Typescript + TailwindCss</p>
+    <div className="bg-gray-400 min-h-screen">
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between h-[50px] text-white bg-gray-500 items-center">
+      <h3 className="font-bold italic">Tech Store</h3>
+      <div>
+        <Link to="/" className="mr-2">Home</Link>
+        <Link to="/favorites">Favorites</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
